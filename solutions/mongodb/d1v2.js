7775956f64d8587be9793bc10afcfe04b22b38bf
Peter Fitch <ptmfitch@users.noreply.github.com>
2022-12-01 15:07:43 +0000

    version 2 of day 1 in mongodb aggregation, uses strings to split rather than complex slicing on indixes, and uses new  accumulator to make part 1 to part 2 as easy as changing 1 value

---
+++ solutions/mongodb/d1v2.js
@@ -0,0 +1,88 @@
+[
+  {
+    $project: {
+      _id: 0,
+      s: {
+        $cond: {
+          if: {
+            $eq: ["$line", ""],
+          },
+          then: "\n",
+          else: {
+            $concat: ["$line", " "],
+          },
+        },
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      ss: {
+        $push: "$s",
+      },
+    },
+  },
+  {
+    $set: {
+      elves: {
+        $let: {
+          vars: {
+            s: {
+              $reduce: {
+                input: "$ss",
+                initialValue: "",
+                in: {
+                  $concat: ["$$value", "$$this"],
+                },
+              },
+            },
+          },
+          in: {
+            $map: {
+              input: { $split: ["$$s", "\n"] },
+              as: "elf",
+              in: {
+                $sum: {
+                  $map: {
+                    input: {
+                      $split: [
+                        {
+                          $rtrim: {
+                            input: "$$elf",
+                            chars: " ",
+                          },
+                        },
+                        " ",
+                      ],
+                    },
+                    as: "cal",
+                    in: { $toInt: "$$cal" },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    $unwind: {
+      path: "$elves",
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      res: {
+        $topN: {
+          output: ["$elves"],
+          sortBy: { elves: -1 },
+          n: 1, // Part 1
+          // n: 3, // Part 2
+        },
+      },
+    },
+  },
+]
