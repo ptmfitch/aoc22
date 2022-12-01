3b37688417cdbc86378e5567894bc06e9c94f3a7
Peter Fitch <ptmfitch@users.noreply.github.com>
2022-12-01 11:57:14 +0000

    day 1 mongodb aggregation solutions added

---
+++ solutions/mongodb/d1p1.js
@@ -0,0 +1,142 @@
+[
+  {
+    $project: {
+      _id: 0,
+      cals: {
+        $cond: {
+          if: {
+            $eq: ["$line", ""],
+          },
+          then: -1,
+          else: {
+            $toInt: "$line",
+          },
+        },
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      cals: {
+        $push: "$cals",
+      },
+    },
+  },
+  {
+    $set: {
+      spaces: {
+        $let: {
+          vars: {
+            inner: {
+              $filter: {
+                input: {
+                  $range: [
+                    0,
+                    {
+                      $size: "$cals",
+                    },
+                  ],
+                },
+                cond: {
+                  $lt: [
+                    {
+                      $arrayElemAt: ["$cals", "$$this"],
+                    },
+                    0,
+                  ],
+                },
+              },
+            },
+          },
+          in: {
+            $concatArrays: [
+              "$$inner",
+              [
+                {
+                  $size: "$cals",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    },
+  },
+  {
+    $set: {
+      elves: {
+        $map: {
+          input: "$spaces",
+          as: "i",
+          in: {
+            $cond: {
+              if: {
+                $eq: [
+                  "$$i",
+                  {
+                    $first: "$spaces",
+                  },
+                ],
+              },
+              then: {
+                $sum: {
+                  $slice: ["$cals", 0, "$$i"],
+                },
+              },
+              else: {
+                $let: {
+                  vars: {
+                    j: {
+                      $indexOfArray: ["$spaces", "$$i"],
+                    },
+                    n: {
+                      $add: [
+                        {
+                          $arrayElemAt: [
+                            "$spaces",
+                            {
+                              $subtract: [
+                                {
+                                  $indexOfArray: [
+                                    "$spaces",
+                                    "$$i",
+                                  ],
+                                },
+                                1,
+                              ],
+                            },
+                          ],
+                        },
+                        1,
+                      ],
+                    },
+                  },
+                  in: {
+                    $sum: {
+                      $slice: [
+                        "$cals",
+                        "$$n",
+                        {
+                          $subtract: ["$$i", "$$n"],
+                        },
+                      ],
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      res: {
+        $max: "$elves",
+      },
+    },
+  },
+]
---
+++ solutions/mongodb/d1p2.js
@@ -0,0 +1,155 @@
+[
+  {
+    $project: {
+      _id: 0,
+      cals: {
+        $cond: {
+          if: {
+            $eq: ["$line", ""],
+          },
+          then: -1,
+          else: {
+            $toInt: "$line",
+          },
+        },
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      cals: {
+        $push: "$cals",
+      },
+    },
+  },
+  {
+    $set: {
+      spaces: {
+        $let: {
+          vars: {
+            inner: {
+              $filter: {
+                input: {
+                  $range: [
+                    0,
+                    {
+                      $size: "$cals",
+                    },
+                  ],
+                },
+                cond: {
+                  $lt: [
+                    {
+                      $arrayElemAt: ["$cals", "$$this"],
+                    },
+                    0,
+                  ],
+                },
+              },
+            },
+          },
+          in: {
+            $concatArrays: [
+              "$$inner",
+              [
+                {
+                  $size: "$cals",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    },
+  },
+  {
+    $set: {
+      elves: {
+        $map: {
+          input: "$spaces",
+          as: "i",
+          in: {
+            $cond: {
+              if: {
+                $eq: [
+                  "$$i",
+                  {
+                    $first: "$spaces",
+                  },
+                ],
+              },
+              then: {
+                $sum: {
+                  $slice: ["$cals", 0, "$$i"],
+                },
+              },
+              else: {
+                $let: {
+                  vars: {
+                    j: {
+                      $indexOfArray: ["$spaces", "$$i"],
+                    },
+                    n: {
+                      $add: [
+                        {
+                          $arrayElemAt: [
+                            "$spaces",
+                            {
+                              $subtract: [
+                                {
+                                  $indexOfArray: [
+                                    "$spaces",
+                                    "$$i",
+                                  ],
+                                },
+                                1,
+                              ],
+                            },
+                          ],
+                        },
+                        1,
+                      ],
+                    },
+                  },
+                  in: {
+                    $sum: {
+                      $slice: [
+                        "$cals",
+                        "$$n",
+                        {
+                          $subtract: ["$$i", "$$n"],
+                        },
+                      ],
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    $unwind: {
+      path: "$elves",
+    },
+  },
+  {
+    $sort: {
+      elves: -1,
+    },
+  },
+  {
+    $limit: 3,
+  },
+  {
+    $group: {
+      _id: null,
+      res: {
+        $sum: "$elves",
+      },
+    },
+  },
+]
